dc256e70056dbd426e424e5069f6f4a8cf5b0d29
20chan <2@0chan.dev>
2019-12-24 17:54:35 +0900

    Wrap apis

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 var tart = new Tart("http://localhost:4000/");
 var createSimulationButtons = $("#createSimulation")[0];
+var selectSimulationButtons = $("#simulations")[0];
 
 function loadModels() {
     createSimulationButtons.innerHTML = "";
     tart.getModels(models => {
         for (var i = 0; i < models.length; i++) {
-            var btn = $("<button>" + models[i] + "</button>");
-            btn.appendTo(createSimulationButtons).click(() => {
-                tart.createSimulation(i, simulId => {
-    
-                });
-            });
+            createButton(models[i], i);
         }
     });
+
+    function createButton(model, i) {
+        var btn = $("<button>" + model + "</button>");
+        btn.appendTo(createSimulationButtons).click(() => {
+            tart.createSimulation(i, simulId => {
+                loadSimulations();
+            });
+        });
+    }
 }
 
 function loadSimulations() {
+    selectSimulationButtons.innerHTML = "";
+    tart.getSimulations(simuls => {
+        for (var i = 0; i < simuls.length; i++) {
+            createButton(simuls[i], i);
+        }
+    });
+
+    function createButton(simul, i) {
+        var btn = $("<button>" + simul + "</button>");
+        btn.appendTo(selectSimulationButtons).click(() => {
+            selectSimulation(i);
+        });
+    }
 }
+
+function selectSimulation(i) {
+
+}
+
+loadModels();
+loadSimulations();
 
 var data = [
     [1, 1, 1, 1],
@@ -25,8 +50,8 @@ var data = [
     [1, 1, 1, 1],
 ];
 
-var sheet = document.getElementById("statTable");
-jexcel(sheet, {
+var table = document.getElementById("statTable");
+var sheet = jexcel(table, {
     data: data,
     columns: [
         {
--- tart.js
+++ tart.js
@@ -3,14 +3,54 @@ function Tart(url) {
 }
 
 Tart.prototype.getModels = function(callback) {
-    $.get(this.url + 'api/models', function(responseText) {
-        callback(responseText);
-    });
+    $.get(this.url + 'api/models', callback);
+}
+
+Tart.prototype.getSimulations = function(callback) {
+    $.get(this.url + 'api/simulations', callback);
 }
 
 Tart.prototype.createSimulation = function(modelIndex, callback) {
-    var body = { modelIndex: modelIndex };
-    $.post(this.url + 'api/simulations', body, function(responseText) {
-        callback(Number(JSON.parse(responseText).id));
+    var body = JSON.stringify({ modelIndex: modelIndex });
+    $.post(this.url + 'api/simulations', body, callback);
+}
+
+Tart.prototype.getSimulation = function(simul, callback) {
+    $.get(this.url + 'api/simulations/' + simul, callback);
+}
+
+Tart.prototype.tickSimulation = function(simul, interval, callback) {
+    $.get(this.url + 'api/simulations/' + simul + '/tick?interval=' + interval, callback);
+}
+
+Tart.prototype.getSimulationUpgradeTypes = function(simul, callback) {
+    $.get(this.url + 'api/simulations/' + simul + '/types', callback);
+}
+
+Tart.prototype.getChoicesOfSimulation = function(simul, callback) {
+    $.get(this.url + 'api/simulations/' + simul + '/choices', callback);
+}
+
+Tart.prototype.getChoiceTypeInfo = function(simul, choice, callback) {
+    $.get(this.url + 'api/simulations/' + simul + '/choices/' + choice + '/type', callback);
+}
+
+Tart.prototype.createChoice = function(simul, choice, callback) {
+    $.post(this.url + 'api/simulations/' + simul + '/choices/' + choice, null, callback);
+}
+
+Tart.prototype.getUpgrades = function(simul, callback) {
+    $.get(this.url + 'api/simulations/' + simul + '/upgrades', callback);
+}
+
+Tart.prototype.getUpgrade = function(simul, upgrade, callback) {
+    $.get(this.url + 'api/simulations/' + simul + '/upgrades/' + upgrade, callback);
+}
+
+Tart.prototype.updateUpgrades = function(simul, upgrade, callback) {
+    $.ajax({
+        url: this.url + 'api/simulations/' + simul + '/upgrades/' + upgrade,
+        type: 'PUT',
+        success: callback
     });
 }
